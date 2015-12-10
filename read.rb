02ec7b80d43f61b4afac0b61eece45e1bb8ab340
Dan Stutzman <dan@danstutzman.com>
2015-12-10 16:53:24 -0700

    Sorting by length.  Also filtering out English.

---
+++ read.rb
@@ -0,0 +1,38 @@
+require 'json'
+
+STDERR.puts "Reading words.en.txt..."
+en_words = {}
+File.open('words.en.txt').each_line do |line|
+  en_words[line.strip.downcase] = true
+end
+
+STDERR.puts "Reading words.es.txt..."
+es_words = {}
+File.open('words.es.txt').each_line do |line|
+  line.strip.split(' ').each do |word|
+    es_words[word.downcase] = true
+  end
+end
+
+STDERR.puts "Reading akwid.txt..."
+lines = []
+File.open('akwid.txt').each_line do |line|
+  object = JSON.parse(line)
+  if object['type'] == 'song_text'
+    lines += object['song_text']
+  end
+end
+
+STDERR.puts "Processing and sorting..."
+
+lines.map! { |line| line.strip }
+lines.reject! { |line| line == '' }
+
+lines.reject! do |line|
+  words = line.downcase.split(/\s/)
+  num_en_words = words.count { |word| en_words[word] }
+  num_es_words = words.count { |word| es_words[word] }
+  num_es_words < num_en_words || (num_es_words < words.size / 2)
+end
+
+puts lines.sort_by { |line| line.length }
